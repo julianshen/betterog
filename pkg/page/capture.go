c5603427123e4d30554aea06076498ff60e4409e
Julian Shen <julianshen@gmail.com>
2021-08-09 02:29:22 +0800

    Revise fullscreencapture

--- pkg/page/capture.go
+++ pkg/page/capture.go
@@ -5,8 +5,36 @@ import (
 	"encoding/base64"
 	"log"
 
+	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
 )
+
+func FullScreenshotInViewport(res *[]byte, quality int) chromedp.EmulateAction {
+	if res == nil {
+		panic("res cannot be nil")
+	}
+	return chromedp.ActionFunc(func(ctx context.Context) error {
+		format := page.CaptureScreenshotFormatJpeg
+
+		var err error
+		// capture screenshot
+		*res, err = page.CaptureScreenshot().
+			WithCaptureBeyondViewport(true).
+			WithFormat(format).
+			WithQuality(int64(quality)).WithClip(&page.Viewport{
+			X:      0,
+			Y:      0,
+			Width:  1200,
+			Height: 630,
+			Scale:  1,
+		}).Do(ctx)
+
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+}
 
 func Capture(encodedurl string) ([]byte, error) {
 	var err error
@@ -29,7 +57,7 @@ func Capture(encodedurl string) ([]byte, error) {
 		if err = chromedp.Run(ctx, chromedp.Tasks{
 			chromedp.EmulateViewport(1200, 630),
 			chromedp.Navigate(url),
-			chromedp.FullScreenshot(&buf, 90),
+			FullScreenshotInViewport(&buf, 90),
 		}); err != nil {
 			return nil, err
 		}
